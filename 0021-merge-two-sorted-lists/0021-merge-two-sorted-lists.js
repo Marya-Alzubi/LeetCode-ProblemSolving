d20025a4f3cac94bfade719ffcaa0da9a51de613
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-18 01:15:18 +0300

    Time: 91 ms (78.89%), Space: 44.5 MB (24.35%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,41 +11,56 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-
-    let headNode ;
-    let tempNode;
-    if(!list1 ){
-        return list2;
-    }else if(!list2){
-        return list1;
-    }
-    if(list1.val < list2.val){
-        headNode= list1;
-        tempNode = headNode;
-        list1=list1.next;
-    }else{
-        headNode= list2;
-        tempNode = headNode;
-        list2=list2.next;
-    }
-    while(list1!==null && list2!==null){
-        if(list1.val < list2.val){
-            tempNode.next = list1;
+   
+    let dummyNode = new ListNode(-10000);
+    let prevNode =dummyNode;
+    while(list1 && list2){
+        if(list1.val<list2.val){
+            prevNode.next=list1;
             list1=list1.next;
         }else{
-            tempNode.next = list2;
-            list2=list2.next;
+            prevNode.next=list2;
+            list2=list2.next;            
         }
-        // ba7arek tempNode evey loop
-        tempNode= tempNode.next;
+        prevNode=prevNode.next;
     }
-    if(list1 ==null){
-        tempNode.next=list2;
-    }
-    if(list2==null){
-        tempNode.next =list1;
-    }
-  return headNode;      
+    if(list1 ==null) prevNode.next=list2;
+    if(list2==null) prevNode.next =list1;
+    return dummyNode.next;
+//    let headNode ;
+//    let tempNode;
+//    if(!list1 ){
+//        return list2;
+//    }else if(!list2){
+//        return list1;
+//   }
+//    if(list1.val < list2.val){
+//        headNode= list1;
+//        tempNode = headNode;
+//        list1=list1.next;
+//    }else{
+//        headNode= list2;
+//        tempNode = headNode;
+//        list2=list2.next;
+//    }
+//    while(list1 && list2){
+//        if(list1.val < list2.val){
+//            tempNode.next = list1;
+//            list1=list1.next;
+//        }else{
+//            tempNode.next = list2;
+//            list2=list2.next;
+//        }
+//       // ba7arek tempNode evey loop
+//        tempNode= tempNode.next;
+//    }
+//    if(list1 ==null) tempNode.next=list2;
+//    if(list2==null) tempNode.next =list1;
+//  return headNode; 
+//
+//    
 };
+//
+//
 //Time Complexity: O(N + M), where N and M are the size of list1 and list2 respectively
 //Space Complexity: O(1)
