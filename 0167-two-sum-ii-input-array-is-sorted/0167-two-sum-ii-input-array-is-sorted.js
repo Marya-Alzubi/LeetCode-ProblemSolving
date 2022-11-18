c74ab8a56f1b85f3a7516ac3323d940ea058a17b
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-18 16:56:04 +0300

    Time: 115 ms (25.19%), Space: 43.2 MB (19.28%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let left = 0;
+    /*let left = 0;
     let right = numbers.length-1;
     //console.log("init right",numbers[right]);
     //console.log("init left",numbers[left]);
@@ -20,23 +20,23 @@ var twoSum = function(numbers, target) {
         }else{
             return [left+1, right+1];
         }
-    }
+    }*/
     
-    /*let left = numbers[0];
+    let left = numbers[0];
     let right = numbers[numbers.length-1];
     for(let i=0,j=numbers.length-1 ;i<numbers.length-1 ; ){
-         console.log("init right",right);
-         console.log("init left",left);
+         //console.log("init right",right);
+         //console.log("init left",left);
         if(left+right > target){
             right = numbers[j-1];
             j--;
-            console.log("right",right);
+            //console.log("right",right);
         }else if(left+right<target){
             left =numbers[i+1];
             i++;
-            console.log("left",left);
+            //console.log("left",left);
         }else if(left+right==target){
             return [i+1,j+1];
         }
-    }*/
+    }
 };
