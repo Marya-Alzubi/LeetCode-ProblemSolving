d08343f026293baa69ae1d72f3fc7f3779d05c76
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-19 00:54:29 +0300

    Time: 212 ms (31.76%), Space: 49.3 MB (21.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,22 +3,12 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    /*let maxLength = 0;
-    let length = 0;
-    if(s.length ==1) return 1;
-    for(let i=0;i<s.length;i++){
-        if(s[i]!=s[i+1]){
-            maxLength++;
-        }
-    }*/
+    ///////////////////// hash table ////////////////////////
     let map ={};
     let maxLength =0;
     let left=0;
     for(let right=0;right<s.length;right++){
        map[s[right]]=( map[s[right]] ||0)+1;
-        /*if(!map[s[right]]){
-            map[s[right]]=1;  
-        }*/
         while(map[s[right]]>1){
             map[s[left]]--;
             left++;
@@ -27,14 +17,22 @@ var lengthOfLongestSubstring = function(s) {
     }
     
     return maxLength;
-    
-    /*t result=[];
-    for(let i=0;i<s.length;i++){
-       if(!result.includes(s[i])){
-            result.push(s[i]);
-       }else{
-           continue;
-       }
+    //
+    //
+    ///////////////////// set [dynamic sliding window size] ////////////////////////  
+    //
+    //
+    /*let set = new Set();
+    let maxLength =0;
+    let left=0;
+    for(let right=0;right<s.length;right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLength=Math.max(maxLength,(right-left+1));
     }
-     return result.length;*/
+    return maxLength;*/
+    
 };
