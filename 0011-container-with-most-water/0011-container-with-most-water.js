99328be265a152f33dee9b37c1a2edf1c5be1b0f
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-18 20:37:04 +0300

    Time: 121 ms (65.64%), Space: 49.1 MB (92.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -6,9 +6,9 @@ var maxArea = function(height) {
     let l =0;
     let r=height.length-1;
     let area=0;
-    while(r>l){
-        if(area<(Math.min(height[l],height[r])*(r-l))){
-            area = Math.min(height[l],height[r])*(r-l);
+    while(r>l){ // when meet get out of the loop
+        if(area<((r-l)*Math.min(height[l],height[r]))){ 
+            area = (r-l)*Math.min(height[l],height[r]); // (length * height) of the blue rectanguler 
         }
         if(height[l]<height[r]){
             l++;
