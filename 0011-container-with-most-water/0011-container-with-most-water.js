90450a0c7e084a91b3eae051391c877de3e42751
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-18 18:34:16 +0300

    Time: 141 ms (27.80%), Space: 49.2 MB (88.75%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -6,15 +6,9 @@ var maxArea = function(height) {
     let l =0;
     let r =height.length-1;
     let Area=0;
-    let minNum;
     while(l<r){
-        if(height[r]>height[l]){
-            minNum= height[l];
-        }else{
-            minNum= height[r];
-        }
-        if(Area < (r-l) * minNum){
-             Area = (r-l)* minNum;
+        if(Area < (r-l) * Math.min(height[l],height[r])){
+             Area = (r-l)* Math.min(height[l],height[r]);
         };
         if(height[l]<height[r]){
              l++;
