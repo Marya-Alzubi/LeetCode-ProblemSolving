4d0b78a8651bc149f4a85679ef675a72fa6f48e2
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-19 23:48:02 +0300

    Time: 124 ms (43.32%), Space: 45.3 MB (34.16%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -18,7 +18,7 @@ var hasCycle = function(head) {
     */ 
     let slow = head;
     let fast = head;
-    while(fast && fast.next && fast.next.next){
+    while(fast && fast.next){
         slow=slow.next;
         fast=fast.next.next;
         if(fast==slow){
