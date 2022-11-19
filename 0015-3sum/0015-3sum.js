cab87be47bfa5b4871a4d3fe3689c0fca6305a30
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-19 23:55:27 +0300

    Time: 4173 ms (7.32%), Space: 64 MB (8.14%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    let obj={}
+    nums=nums.sort((a,b)=>a-b)
+    // console.log(nums)
+    for(let start=0;start<nums.length;start++){
+        let right=nums.length-1
+        let left=start+1
+        while(left<right){
+            if(nums[left]+nums[right]+nums[start]==0){
+                // console.log(start,left,right)
+                // arr.push([nums[start],nums[left],nums[right]])
+                obj[[nums[start],nums[left],nums[right]]]=[nums[start],nums[left],nums[right]]
+                left++
+                right--
+            }else if(nums[left]+nums[right]+nums[start]>0){
+                right--
+            }else if(nums[left]+nums[right]+nums[start]<0){
+                left++
+            }
+        }
+    }
+     return (Object.values(obj))
+};
