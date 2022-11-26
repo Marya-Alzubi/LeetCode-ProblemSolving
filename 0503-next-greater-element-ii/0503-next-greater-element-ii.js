ecd9667a48a51e41e367e81782ec0ad7e62e9993
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-26 13:28:13 +0300

    Time: 167 ms (70.95%), Space: 47.7 MB (76.03%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -18,7 +18,7 @@ var nextGreaterElements = function(nums) {
     //console.log("stack1",stack);
     //console.log("next1",next);
     
-    // to find the next greater for the rest of our stack (circuler idea)
+    // to find the next greater for the rest of our stack (circuler idea) // now we start with NOT an empty stack -> so we can find the next greater for the rest as circuler -> the while will stop when reach the same number as a worst case senario
     for(let i=0;i<nums.length;i++){
         element=nums[i];
         while(stack.length && element > nums[stack[stack.length-1]]){
@@ -41,19 +41,22 @@ var nextGreaterElements = function(nums) {
     //console.log(next);
     //console.log(stack);
     return result;*/
-    let stack = [];
-    let result=[];
-    for(let i=0;i<nums.length;i++){
+    //////////////////////////////////////without the last for //////////////////////////
+    let stack = []; // since the array may has duplicated number, in this case it's better to store index number insted of the value
+    let result=new Array(nums.length).fill(-1); // I need to give an intial value -1 for the whole numbers // then if the number has a next greater I will change it
+    /*for(let i=0;i<nums.length;i++){
         result.push(-1);
+    }*/
+    // create our stack as usual
+    for(let i=0;i<nums.length;i++){ 
+        element=nums[i]; // 1   //2
+        while( stack.length && element > nums[stack[stack.length-1]]){ // __no__  // 2 > 1 nums[0]
+            let popped = stack.pop(); //    // 0
+            result[popped]=element;   //    // result=[2,-1,-1,-1,-1]
     }
-    for(let i=0;i<nums.length;i++){
-        element=nums[i];
-        while( stack.length && element > nums[stack[stack.length-1]]){
-            let popped = stack.pop();
-            result[popped]=element;
+        stack.push(i); // [0]  //[1]                                      
     }
-        stack.push(i);                                         
-    }
+    // to find the next greater for the rest of our stack (circuler idea) // now we start with NOT an empty stack -> so we can find the next greater for the rest as circuler -> the while will stop when reach the same number as a worst case senario
     for(let i=0;i<nums.length;i++){
         element=nums[i];
         while(stack.length && element > nums[stack[stack.length-1]]){
