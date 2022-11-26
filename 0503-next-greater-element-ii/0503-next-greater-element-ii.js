1fb818db11b851545c0fb08e59cbb4f238006720
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-26 13:05:36 +0300

    Time: 169 ms (69.84%), Space: 48.7 MB (24.44%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -3,9 +3,10 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
-    let stack=[];
+    /*let stack=[]; // since the array may has duplicated number, in this case it's better to store index number insted of the value
     let next = {};
     let result =[];
+    // create our stack as usual
     for(let i=0;i<nums.length;i++){
         element=nums[i];
         while( stack.length && element > nums[stack[stack.length-1]]){
@@ -16,6 +17,8 @@ var nextGreaterElements = function(nums) {
     }
     //console.log("stack1",stack);
     //console.log("next1",next);
+    
+    // to find the next greater for the rest of our stack (circuler idea)
     for(let i=0;i<nums.length;i++){
         element=nums[i];
         while(stack.length && element > nums[stack[stack.length-1]]){
@@ -24,7 +27,8 @@ var nextGreaterElements = function(nums) {
         }
     }
     //console.log("stack2",stack);
-    //console.log("next2",next);   
+    //console.log("next2",next);
+    // decide what to push in the result array
     for(let i=0;i<nums.length;i++){
         element=nums[i];
         if(next[i] !== undefined){
@@ -36,5 +40,26 @@ var nextGreaterElements = function(nums) {
     
     //console.log(next);
     //console.log(stack);
+    return result;*/
+    let stack = [];
+    let result=[];
+    for(let i=0;i<nums.length;i++){
+        result.push(-1);
+    }
+    for(let i=0;i<nums.length;i++){
+        element=nums[i];
+        while( stack.length && element > nums[stack[stack.length-1]]){
+            let popped = stack.pop();
+            result[popped]=element;
+    }
+        stack.push(i);                                         
+    }
+    for(let i=0;i<nums.length;i++){
+        element=nums[i];
+        while(stack.length && element > nums[stack[stack.length-1]]){
+            let popped = stack.pop();
+            result[popped]=element;
+        }
+    }
     return result;
 };
