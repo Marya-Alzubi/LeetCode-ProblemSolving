67d0677453f2d718ef351d290cb656862370c9b9
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-30 02:29:54 +0300

    Time: 118 ms (11.57%), Space: 42.6 MB (62.34%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -5,7 +5,7 @@
 var uniqueOccurrences = function(arr) {
     // counting! so it's frequency array! 
     let frequency ={};
-    let result=[];
+    let values=[];
     for(let i=0;i<arr.length;i++){
         let element =arr[i];
         if(!frequency[element]){
@@ -16,22 +16,35 @@ var uniqueOccurrences = function(arr) {
     }
     //console.log(frequency);
 
+    
+   ////////////////the whole idea is here how to check the values it self of the frequency object? I wil loop throgh the frequency,    once I found a duplicated value I will return false;//////////////////    
+    for(let key in frequency){ 
+        if (values.includes(frequency[key])){
+            return false;
+        }
+        values.push(frequency[key]);
+    }
+    return true;
+
+    
+    
+    
+    
+    //let values =Object.values(frequency);  // I didn't find a usefull solution with it
+
+    //
+    //
     // I tried to loop through the values but I failed
+    //
     /*for(let i =0;i<Object.keys(frequency).length;i++){
         //console.log(Object.values(frequency).includes(frequency[key]));
         if(Object.values(frequency).indexOf(frequency[i][0])!= i && Object.values(frequency).includes(frequency[key]) ){
             return false;
         }
     }*/
-    //////////////// the whole idea is here how to check the values it self of the frequency object? I wil loop throgh the frequency , once I found a duplicated value I will return false;//////////////////
+   //
+   //
+   //
 
-    //let values =Object.values(frequency);  // I didn't find a usefull solution with it
-    
-    for(let key in frequency){ 
-        if (result.includes(frequency[key])){
-            return false;
-        }
-        result.push(frequency[key]);
-    }
-    return true;
+
 };
