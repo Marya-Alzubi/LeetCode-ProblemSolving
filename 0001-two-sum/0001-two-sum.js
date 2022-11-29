a5e159bda3135de0ac735b7390a71c576b9a7660
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-29 23:15:12 +0300

    Time: 75 ms (93.54%), Space: 43.2 MB (25.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -9,14 +9,14 @@ var twoSum = function(nums, target) {
     let map = new Map();
     for(let i=0;i<nums.length;i++){
         let element=nums[i];
-        let subtract = target-element;    //what is the number that I need it to reach the target?
+        let subtract = target-element;    //what is the number (the perfect pair) that I need it to reach the target?
         if(map.has(subtract)){            // is it (the perfect pair) in the previous keys in our map?
             return [i, map.get(subtract)] // yaaay! 
         }else{
-            map.set(element,i)            // store the number in the map maybe I will find its pair!
+            map.set(element,i)            // no :( --> store the number in the map maybe I will be the perfect pair for another one!
         }
     }
-    //////////////////////////////////// the dictionery object ////////////////////////////////
+    //////////////////////////////////// the dictionery object (same logic) ////////////////////////////////
 
     /*let hash ={};
     for(let i=0;i< nums.length;i++){
