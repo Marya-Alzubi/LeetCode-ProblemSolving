0bb8640cb65e9908f48444c82c6418ee79f96f56
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-29 19:58:35 +0300

    Time: 97 ms (83.54%), Space: 43.3 MB (25.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    ////////////////////////////////////// the built in map function /////////////////////////
+    // the idea to find a solution of O(N) so I need to create 1 loop
     let map = new Map();
     for(let i=0;i<nums.length;i++){
         let element=nums[i];
-        let subtract = target-element;
-        if(map.has(subtract)){
-            return [i, map.get(subtract)]
+        let subtract = target-element;    //what is the number that I need it to reach the target?
+        if(map.has(subtract)){            // is it (the perfect pair) in the previous keys in our map?
+            return [i, map.get(subtract)] // yaaay! 
         }else{
-            map.set(element,i)
+            map.set(element,i)            // store the number in the map maybe I will find its pair!
         }
     }
+    //////////////////////////////////// the dictionery object ////////////////////////////////
+
+    /*let hash ={};
+    for(let i=0;i< nums.length;i++){
+        let element=nums[i];
+        let needed = target -element;
+        if(hash.hasOwnProperty(needed)){
+            return [i, hash[needed]]
+        }else{
+            hash[element]=i;
+        }
+    }*/
 };
