bc8fbe5ae18363c945f578116e8c127d5c983cc5
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-11-30 02:26:08 +0300

    Time: 60 ms (96.93%), Space: 42.8 MB (46.04%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
+    // counting! so it's frequency array! 
     let frequency ={};
     let result=[];
     for(let i=0;i<arr.length;i++){
@@ -13,16 +14,22 @@ var uniqueOccurrences = function(arr) {
             frequency[element]++;
         }
     }
-    console.log(frequency);
+    //console.log(frequency);
+
+    // I tried to loop through the values but I failed
     /*for(let i =0;i<Object.keys(frequency).length;i++){
         //console.log(Object.values(frequency).includes(frequency[key]));
         if(Object.values(frequency).indexOf(frequency[i][0])!= i && Object.values(frequency).includes(frequency[key]) ){
             return false;
         }
     }*/
-    for(let key in frequency){
+    //////////////// the whole idea is here how to check the values it self of the frequency object? I wil loop throgh the frequency , once I found a duplicated value I will return false;//////////////////
+
+    //let values =Object.values(frequency);  // I didn't find a usefull solution with it
+    
+    for(let key in frequency){ 
         if (result.includes(frequency[key])){
-            return false
+            return false;
         }
         result.push(frequency[key]);
     }
