e277b0c4dcdfd31f4d43ba602c59f39678c540fa
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-12-03 15:55:06 +0300

    Time: 90 ms (67.11%), Space: 42.7 MB (64.17%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -4,28 +4,41 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
+// The idea in how can I reach the letters in every words
+// COMPARE EVERY 2 WORDS TOGETHER  (first loop)
+// compare the FIRST LETTER ONLY for each word (inside loop)
+// if they are NOT the SAME && Word1[letter1]> word2[letter2] ->return false
+// if they are NOT the SAME && every thing is okay (break)+(i++) "the happy senario" "test the next word"
+// if they are the SAME then compare THE NEXT BESIDE LETTER IN EVERY WORD (j++) "looping until I find 2 letter I can compare between them"
+    
+//edge case: word2.length<word1.length ->return false
+    
+    
     let map= new Map();
     for(let i=0;i<order.length;i++){
         let element=order[i];
         map.set(element,i);
     }
     //console.log(map);
-    for(let i=0;i<words.length-1;i++){
+    for(let i=0;i<words.length-1;i++){ //the last element doesn't have i+1 // excluded it
         let word1=words[i];
         let word2=words[i+1];
         for(let j=0;j<word1.length;j++){
-            if(j==word2.length){
+            if(j==word2.length){  //edge case ["apple","app"]
                 return false;
             }
             let letter1=word1[j];
             let letter2=word2[j];
             if(map.get(letter1)!==map.get(letter2)){
                 if(map.get(letter1)>map.get(letter2)){
-                    return false;
+                    return false; //worst senario
                 }
-                break; 
+                                  // happy senario ---> how to jump to the next pair?
+                break; //the first "not equal letters" of the the words are NOT matching the condition of: (word1>word2)  // so everything is okay here -> let's test the next pair
             }
-            /*else if(map.get(letter1)<map.get(letter2)){
+            // I can't do this because I want to test the whole words 
+            // maybe in the first 2 words is true but the next pair is not!
+            /*else if(map.get(letter1)<map.get(letter2)){ 
                 return true;
                 break;
             }*/
