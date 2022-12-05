73eff5f873f6d7b016f908395916b9dbeffdc24e
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2022-12-05 20:11:00 +0300

    Time: 92 ms (71.24%), Space: 45.3 MB (32.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -7,22 +7,20 @@ var search = function(nums, target) {
     let left = 0;
     let right= nums.length-1;
     let middle;
-    middle= Math.floor((right+left)/2);
-    
     while(right>=left){
         //console.log("left index",left);
         //console.log("right index",right);
-        middle= Math.floor((right+left)/2);
+        middle= Math.floor(left+(right-left)/2);
         //console.log("middle index",middle);
         //console.log("meduim value",nums[middle]);
         if(target==nums[middle]){
             return middle;
         }else if(nums[middle]<target){
             left=middle+1;
-            console.log("new left",left);
+            //console.log("new left",left);
         }else if(nums[middle]>target){
             right=middle-1;
-            console.log("new right",right);
+            //console.log("new right",right);
         }
     }
     
