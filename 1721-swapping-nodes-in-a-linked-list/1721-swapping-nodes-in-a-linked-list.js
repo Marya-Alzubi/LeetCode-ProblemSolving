8da496671e6b8a59210053d78e274ce4bd885eaf
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-03-04 22:10:35 +0300

    Time: 524 ms (51.64%), Space: 78.7 MB (40.73%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.js
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.js
@@ -10,50 +10,89 @@
  * @param {number} k
  * @return {ListNode}
  */
+
+ /**
+ they said swap the values :)))))
+
+  [1,  2,  3,  4,  5]    k=2
+       |       |
+  [1,  4,  3,  2,  5]
+  */
 var swapNodes = function(head, k) {
+    let length = 0;
+    let curr = head;
+
+    //find the length
+    while (curr) {
+        length++;
+        curr = curr.next;
+    }
+
+    //edge case
+    if (k > length) {
+        return head;
+    }
+
+    //find first
+    let first = head;
+    for (let i = 1; i <= k - 1; i++) {
+        first = first.next;
+    }
+
+    //find second
+    let second = head;
+    for (let i = 1; i <= length - k; i++) {
+        second = second.next;
+    }
+
+    //swap the values
+    [first.val, second.val] = [second.val, first.val];
+
+    return head;
+    /////////////// did not work :( -> I tried to change the pointer //////////////
+    // if(!head.next) return head;
     // let prev=null;
     // let next=null;
-    // let i=1;
+    // let length=0;
+    // let count=0;
     // let current=head;
+    // let nodeBefore;
+    // let theFirstSwappingNode;
+    // let theInnerNode;
+    // let theLastInnerNode;
+    // let theSecondSwappingNode;
     // while(current){
-        
+    //     prev=current;
+    //     current=current.next;
+    //     length++;
     // }
-    let start = head;
-    let len = 0, l, count=0, first, second;
-
-    if(!head.next) return head;
-
-// obtain the required node value from end of the list
-    while(start){
-        start = start.next;
-        len++;
-    }
-    l = len-k;
-
-    if(k === l+1) return head;
-
-// extract the values in required nodes of the list
-   let begin = head;
-   while(begin){
-        count++;
-        if(count === k){
-            first = begin.val;
-        } else if(count === l+1){
-            second = begin.val;
-        }
-        begin = begin.next;
-    }
-
-// swapping the node values
-	begin = head;
-	count = 0;
-
-    while(begin){
-        count++;
-        if(count === k) begin.val = second;
-        else if(count === l+1) begin.val = first;
-        begin = begin.next;
-    }
-    return head;  
+    // if(k === length+1) return head;
+    // console.log(length)
+    // current=head;
+    // prev=null;
+    // while(current){
+    //     count++;
+    //     if(count==k){
+    //         nodeBefore=prev;
+    //         theFirstSwappingNode=current;
+    //         theInnerNode=current.next; 
+    //     }
+    //     else if(count==(length-k)) theLastInnerNode=current.next;
+    //     else if(count==(length-k+1)){
+    //         console.log("passed");
+    //         console.log(current);
+    //         theSecondSwappingNode=current;
+    //     }  
+    //     prev=current;
+    //     current=current.next;      
+    // }
+    //
+    // nodeBefore.next=theSecondSwappingNode;
+    // theSecondSwappingNode.next=theInnerNode;
+    // if(theLastInnerNode){   
+    //     theLastInnerNode.next=theFirstSwappingNode;
+    // }
+    //
+    //return head
 
 };
