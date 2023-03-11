f9d96e49bd1007ce17c4cba615869c160d8cf7be
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-03-12 00:39:41 +0300

    Time: 97 ms (34.11%), Space: 47.5 MB (18.60%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ /**
+ like the least frequent
+ 
+ hashMap={ 
+            a: 2,
+            b: 1 
+        }
+mpq = {
+            element:a, priority:2,
+            element:b, priority:1,
+} 
+
+ current & onHold
+
+  */
+var reorganizeString = function(s) {
+    //1.hashMap
+    let hashMap ={};
+    for(let i=0;i<s.length;i++){
+        const element=s[i];
+        if(hashMap[element]){
+            hashMap[element]++;
+        }else{
+            hashMap[element]=1;
+        }
+    }
+    console.log(hashMap);
+
+
+    //2. MaxPriorityQueue
+    const mpq =new MaxPriorityQueue();
+    for(const key in hashMap){
+        mpq.enqueue(key,hashMap[key]);
+    }
+
+    //3. result
+    let result="";
+    let onHold=null;
+    while(mpq.size()){
+        let current = mpq.dequeue();
+        result+=current.element;
+        if(onHold){
+            mpq.enqueue(onHold.element,onHold.priority)
+        }
+        current.priority--;
+        if(current.priority>0){
+            onHold=current
+        }else{
+            onHold=null
+        }
+    }
+
+    if(onHold) return "";
+
+    return result;
+};
