992c9655bc3a960395cb0a81e98e86c5de4ce7d6
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-03-31 17:50:56 +0300

    Time: 64 ms (80.22%), Space: 43.7 MB (57.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -5,27 +5,41 @@
 /**
 T: O(N)
 S: O(N)
+-------------------------
+we can solve it using: Boyer-Moore
+T: O(N)
+S: O(1)
+
  */
 var majorityElement = function(nums) {
-    let max=0;
+    let maxCount=0;
     let result;
 
-    let hashMap = {}
+    // let hashMap = {}
+    // for(let i=0;i<nums.length;i++){
+    //     const element = nums[i];
+    //     if(hashMap[element]){
+    //         hashMap[element]++;
+    //     }else{
+    //         hashMap[element]=1;
+    //     }
+    // }
+
+    // //search for the maximum value 
+    // for(const key in hashMap){
+    //     if(hashMap[key]>maxCount){
+    //         maxCount=hashMap[key]
+    //         result=key
+    //     }
+    // }
+
+    //Boyer-Moore
     for(let i=0;i<nums.length;i++){
         const element = nums[i];
-        if(hashMap[element]){
-            hashMap[element]++;
-        }else{
-            hashMap[element]=1;
-        }
+        if(maxCount===0) result=element;
+        if(element===result) maxCount++
+        else maxCount--;
     }
 
-    //search for the maximum value 
-    for(const key in hashMap){
-        if(hashMap[key]>max){
-            max=hashMap[key]
-            result=key
-        }
-    }
     return result;
 };
