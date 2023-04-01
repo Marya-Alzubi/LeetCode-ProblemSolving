327058417c4972f34d754e0e916215cbccfd8cba
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-04-01 16:36:18 +0300

    Time: 64 ms (57.98%), Space: 42.8 MB (15.22%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ /**
+ [7,     1,      5,      3,      6,       4]          min      max        profit
+  i                                                    7        7            0
+         i                                             1        1            0+0
+                 i                                     1        5            0+4
+                         i                            *3        3             
+                                 i                     3        6            4+3=7  
+                                          i            4        4     
+ --------------------------------------------                                 
+ [1,     2,      3,      4,      5]                               
+  i                                                    1       1           0
+         i                                             1       2           0+1           
+                 i                                     2       3           0+1+1
+                         i                             3       4           0+1+1+1
+                                 i                     4       5           0+1+1+1+1
+  */
+var maxProfit = function(prices) {
+    let max = -Infinity
+    let min = Infinity
+    let profit=0;
+    for(let i=0;i<prices.length;i++){
+        const price= prices[i];
+        if(price<min){
+            min=price
+            max=price
+        }
+        max=Math.max(max,price);
+        if(max>min){
+            profit+=(max-min);
+            min=price;
+        }
+    }
+    return profit;
+};
