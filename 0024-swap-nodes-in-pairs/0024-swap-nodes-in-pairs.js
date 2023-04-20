c8080f3c4fbcea39883bd6d2fca1a9eeb3280d9b
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-04-20 23:55:34 +0300

    Time: 46 ms (98.19%), Space: 42.5 MB (23.26%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,10 +10,10 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    //(0,head) => edge case => if while did not work because there is current but there is NO current.next
+    if(!head||!head.next)return head
     let dummy=new ListNode(0,head);
-    let prev=dummy
-    let current=head
+    let prev=dummy;
+    let current=head;
     while(current && current.next){
         let nextPair=current.next.next;
         let secondNode=current.next
@@ -27,22 +27,22 @@ var swapPairs = function(head) {
         current=nextPair; //1->3 //will not reach 2 & 4
     }
     return dummy.next
-
-    //////////////////////// did not work :(
-    // if(!head)return null;
-    // if(!head.next) return head;
-    // let current=head;
-    // prev=null;
-    // let i=0;
-    // while(current && current.next){
-    //     i++;
-    //     if(i%2==0){
-    //         current.next=prev;
-    //         prev.next=current.next.next;
-    //     }
-    //     console.log(current)
-    //     prev=current;
-    //     current=current.next;
-    // }
-    // return head.next;
 };
+    // //(0,head) => edge case => if while did not work because there is current but there is NO current.next
+    // let dummy=new ListNode(0,head);
+    // let prev=dummy
+    // let current=head
+    // while(current && current.next){
+    //     let nextPair=current.next.next;
+    //     let secondNode=current.next
+
+    //     //reverse
+    //     secondNode.next=current;
+    //     current.next=nextPair;
+    //     prev.next=secondNode; //dummy->2
+
+    //     prev=current
+    //     current=nextPair; //1->3 //will not reach 2 & 4
+    // }
+    // return dummy.next
+
