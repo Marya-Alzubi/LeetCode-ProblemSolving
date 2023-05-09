1bedc29a9c996fff873d4d47c50cf3e85e129589
Marya <71829413+Marya-Alzubi@users.noreply.github.com>
2023-05-10 00:31:32 +0300

    Time: 131 ms (55.10%), Space: 52.5 MB (87.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -15,28 +15,40 @@ sortedStrs = ['aet','aet','ant','aet','ant','abt']
        }
     
      */
-    //sort the chars inside every element
-    let sortedStrs =[];
-    for(let i=0;i<strs.length;i++){// T: m
-        //console.log(strs[i])
-        sortedStrs[i]=strs[i].split('').sort((a, b) => a.localeCompare(b)).join("");//T: nlogn
-    }
-    //console.log(sortedStrs)
-
-    //based the sorted array .. store the values inside the map
     let map={};
-    for(let i=0;i<sortedStrs.length;i++){ //T: m
-        //console.log("sortedStrs[i]",sortedStrs[i])
-        if(!map[sortedStrs[i]]){        
-            map[sortedStrs[i]]=[strs[i]];
+    for(let str of strs){ //T: m 
+        let sortedStr = str.split("").sort().join("") //T: nlogn
+        if(!map[sortedStr]){
+             map[sortedStr] = [str]
         }else{
-            map[sortedStrs[i]].push(strs[i])
-        }
+            map[sortedStr].push(str)
+        }   
     }
-    //console.log(map)
+    return Object.values(map)
+    
+    ///////////////////////////////////////////////////// another sol 
+    // //sort the chars inside every element
+    // let sortedStrs =[];
+    // for(let i=0;i<strs.length;i++){// T: m
+    //     //console.log(strs[i])
+    //     sortedStrs[i]=strs[i].split('').sort((a, b) => a.localeCompare(b)).join("");//T: nlogn
+    // }
+    // //console.log(sortedStrs)
+
+    // //based the sorted array .. store the values inside the map
+    // let map={};
+    // for(let i=0;i<sortedStrs.length;i++){ //T: m
+    //     //console.log("sortedStrs[i]",sortedStrs[i])
+    //     if(!map[sortedStrs[i]]){        
+    //         map[sortedStrs[i]]=[strs[i]];
+    //     }else{
+    //         map[sortedStrs[i]].push(strs[i])
+    //     }
+    // }
+    // //console.log(map)
 
 
-    return Object.values(map);
+    // return Object.values(map);
     //time complixity: O(m*n*logn)
     //space complixity O(n) // why without sortedStr?
 
